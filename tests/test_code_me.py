9d11ec7e359403fc6b6e2bb240539283225aacb4
ayanacharyya <acharyyaayan@gmail.com>
2017-12-12 15:27:59 +1100

    retrying tests

--- tests/test_code_me.py
+++ tests/test_code_me.py
@@ -3,9 +3,9 @@ from some_module import extract_sentence_containing_word
 
 # testing sentence method: simple case
 def test_sentence_method_simple():
-    assert extract_sentence_containing_word('This day. I like. You hungry?', 'like') == '[I like]'
+    assert extract_sentence_containing_word('This day. I like. You hungry?', 'like') == ['I like']
 
 
 # testing sentence method: question case
 def test_sentence_method_question():
-    assert extract_sentence_containing_word('This day. I like. You hungry?', 'hungry') == '[You hungry]'
+    assert extract_sentence_containing_word('This day. I like. You hungry?', 'hungry') == ['You hungry']
