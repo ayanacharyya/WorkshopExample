bf02f5335cf8d092733baff35d21254a97043598
ayanacharyya <acharyyaayan@gmail.com>
2017-12-12 13:59:01 +1100

    trying to break it

--- some_module/code.py
+++ some_module/code.py
@@ -10,6 +10,7 @@ import numpy as np
 
 def integrate_trapz(xs, ys):
     """ Document and test me"""
+    xs = int(xs)
     widths = np.diff(xs)
     midpoints = 0.5 * (ys[1:] + ys[:-1])
     area = np.sum(widths * midpoints)
