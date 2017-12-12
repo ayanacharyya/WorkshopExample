bab2059cfd1538611e9dbe0a0f098da76d15b377
ayanacharyya <acharyyaayan@gmail.com>
2017-12-12 14:19:18 +1100

    trying new tests

---
+++ tests/test_code_me.py
@@ -0,0 +1,11 @@
+from some_module import extract_sentence_containing_word
+
+
+# testing sentence method: simple case
+def test_sentence_method_simple():
+    assert extract_sentence_containing_word('This day. I like. You hungry?', 'like') == 'I like'
+
+
+# testing sentence method: question case
+def test_sentence_method_question():
+    assert extract_sentence_containing_word('This day. I like. You hungry?', 'hungry') == 'You hungry'
